783e92da0807928b7faebe55da6d8dc07580879f
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-09-30 23:31:40 +0530

    server created

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const name = 'kadar'
-console.log(kadar)
+import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express'
+const app = express()
+const port = process.env.port || 5000
+
+
+app.listen(port, ()=>{
+    console.log(`server started at port ${port}` )
+})
