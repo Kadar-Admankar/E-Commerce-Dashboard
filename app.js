0b356f4ecf64d8d8ebbaee571b6b4220d03d6041
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-10-08 23:20:32 +0530

    cors added

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const dotenv = require('dotenv')
 dotenv.config()
 require('./db/config.js')
+const cors = require('cors')
 const express = require('express')
 const User = require('./db/User.js')
 const app = express()
 const port = process.env.port || 5000
 
 app.use(express.json())
+app.use(cors())
 app.post('/register', async (req, res)=>{
     let user = new User(req.body)
     const result = await user.save()
