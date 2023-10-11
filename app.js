f96df7a3b807abec62f80f7e23fc7d13c2c985c5
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-10-11 06:45:50 +0530

    login api created

--- app.js
+++ app.js
@@ -9,12 +9,30 @@ const port = process.env.port || 5000
 
 app.use(express.json())
 app.use(cors())
+
 app.post('/register', async (req, res)=>{
     let user = new User(req.body)
-    const result = await user.save()
-   res.send(result)
+    let result = await user.save()
+    result = result.toObject()
+    console.log('result11', result)
+    delete result.password
+   res.send(result)   // we can take response from postman by writing res.send(). other method is res.json()
 })
 
+app.post('/login', async (req, res)=>{
+    if(req.body.email && req.body.password){
+        let user = await User.findOne(req.body).select('-password')
+        if(user){
+            res.send(user)
+        }else{
+            res.send({ message: "No User Found" })
+        }
+    }else{
+        res.send({ message: "No User Found" })
+    }
+    
+     
+})
 
 app.listen(port, ()=>{
     console.log(`server started at port ${port}` )
