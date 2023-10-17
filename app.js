c690d5da2a07719fa4ad1bf2c7f6a6d4802bde94
Kadar Admankar <admankarkadar@sggs.ac.in>
2023-10-17 23:56:00 +0530

    product update api

--- app.js
+++ app.js
@@ -57,6 +57,23 @@ app.delete('/product/:id', async (req, res)=>{
     res.send(result)
 })
 
+app.get('/product/:id', async (req, res)=>{ // for above api also endpoint is same, but method is diff, so no issue
+    let result = await Product.findOne({'_id':req.params.id})
+    if(result){
+        res.send(result)
+    }else{
+        res.send({message:"No Record Found"})
+    }
+})
+
+app.put('/product/:id', async (req, res)=>{
+    let result = await Product.updateOne(
+        { '_id': req.params.id},
+        { $set: req.body }
+        )
+        res.send(result)
+})
+
 app.listen(port, ()=>{
     console.log(`server started at port ${port}`)
 })
